13d9c1b384ae1651695428625c43749dba2fae19
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-03 12:43:04 -0300

    I created a method to handle multiple events

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        
     }
     initialize() {
         this.setDisplayDateTime();
@@ -17,17 +18,31 @@ class CalcController {
         }, 1000);
     }
     //------------buttons   
+
+    addEventListenerAll(element, events,fn){
+
+        events.split(' ').forEach(event =>{
+            element.addEventListener(event,fn, false);
+        });
+
+    }
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
 
         buttons.forEach((btn ,index)=> {
-            btn.addEventListener('click', e =>{
+            this.addEventListenerAll(btn,"click drag", e =>{
 
                 console.log(btn.className.baseVal.replace("btn-",""));
     
             });
+
+            this.addEventListenerAll(btn,"mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            })
+
         })
 
     }
