95d740587ece23cc3b49c25091fc2eaef231a3e0
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-03 10:25:12 -0300

    I created an event to modify the value

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,20 +2,33 @@ class CalcController {
 
 constructor(){
     this._displayCalc = "0";
-    this._dataAtual;
+    this._currentDate;
+    this.initialize();
 }
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+
+        let DateEl= document.querySelector("#data");
+
+        let timeEl= document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        DateEl.innerHTML = "21/05/2023";
+        timeEl.innerHTML = "00:00";
+
+    }
+//------------------
     get displayCalc(){
         return this._displayCalc
     }
-
     set displayCalc(valor){
         this._displayCalc = valor;
     }
-
-    get dataAtual (){
-
+//------------------
+    get currentDate (){
+        return this._currentDate;
     }
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 }
