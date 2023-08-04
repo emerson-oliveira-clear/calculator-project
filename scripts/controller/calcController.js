6d759c251cd2f2f65b3b1faec53be467e3c41bfd
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-04 09:34:30 -0300

    created methods to identify numbers and operators

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
     constructor() {
+        this._operation = [];
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display");
         this._DateEl = document.querySelector("#data");
@@ -8,7 +9,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
-        
+
     }
     initialize() {
         this.setDisplayDateTime();
@@ -19,27 +20,143 @@ class CalcController {
     }
     //------------buttons   
 
-    addEventListenerAll(element, events,fn){
+    addEventListenerAll(element, events, fn) {
 
-        events.split(' ').forEach(event =>{
-            element.addEventListener(event,fn, false);
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
         });
 
     }
 
-    initButtonsEvents(){
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1]
+
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;    
+    }
+
+    isOperator(value) {
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1)
+    }
+
+    addOperation(value) {
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)){
+
+                console.log(value)
+
+            }else{
+                
+                this._operation.push(value);
+
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        
+        console.log(this._operation);
+    }
+
+    SetError() {
+        this.displayCalc = "Error";
+    }
+
+
+
+    execBtn(value) {
+
+        switch (value) {
+            case "ac":
+                this.clearAll();
+                break;
+
+            case "ce":
+                this.clearEntry();
+                break;
+
+            case "soma":
+                this.addOperation('+');
+                break;
+
+            case "subtracao":
+                this.addOperation('-');
+                break;
+
+            case "divisao":
+                this.addOperation('/');
+                break;
+
+            case "multiplicacao":
+                this.addOperation('*');
+                break;
+
+            case "porcento":
+                this.addOperation('%');
+                break;
+
+            case "igual":
+
+                break;
+
+            case "ponto":
+                this.addOperation('.');
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.SetError();
+                break;
+
+        }
+
+    }
+
+    initButtonsEvents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
 
-        buttons.forEach((btn ,index)=> {
-            this.addEventListenerAll(btn,"click drag", e =>{
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""));
-    
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
+
             });
 
-            this.addEventListenerAll(btn,"mouseover mouseup mousedown", e => {
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
                 btn.style.cursor = "pointer";
             })
 
@@ -53,7 +170,7 @@ class CalcController {
             day: "2-digit",
             month: "long",
             year: "numeric"
-        
+
         })
 
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
@@ -87,6 +204,6 @@ class CalcController {
         this._currentDate = valor;
     }
     set displayCalc(value) {
-        this._displayCalcEl.innerHTML = valor;
+        this._displayCalcEl.innerHTML = value;
     }
 }
