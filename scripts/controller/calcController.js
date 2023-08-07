26bb3c9e1e33761041061a91b2d9b73f47f28686
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-08-07 08:22:05 -0300

    keyboard interactive with events

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -11,6 +11,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyBoard();
 
     }
     initialize() {
@@ -22,7 +23,57 @@ class CalcController {
 
         this.setLastNumberToDisplay()
     }
-    //------------buttons   
+
+    initKeyBoard() {
+        document.addEventListener('keyup', e => {
+            console.log();
+
+            switch (e.key) {
+                case "Escape":
+                    this.clearAll();
+                    break;
+
+                case "Backspace":
+                    this.clearEntry();
+                    break;
+
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "%":
+
+                    this.addOperation(e.key);
+                    break;
+
+
+                case "Enter":
+                case "=":
+                    this.calc();
+                    break;
+
+                case ".":
+                case ",":
+                    this.addDot();
+                    break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+        });
+    }
+
+
 
     addEventListenerAll(element, events, fn) {
 
@@ -132,13 +183,13 @@ class CalcController {
             }
 
         }
-            if (!lastItem) {
+        if (!lastItem) {
 
-                lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
 
-            }
-            return lastItem;
-        
+        }
+        return lastItem;
+
     }
 
     setLastNumberToDisplay() {
@@ -177,7 +228,7 @@ class CalcController {
                 }
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation((newValue));
 
 
                 this.setLastNumberToDisplay();
@@ -195,6 +246,11 @@ class CalcController {
 
     addDot() {
         let lastOperation = this.getLastOperation();
+
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
+
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
         } else {
